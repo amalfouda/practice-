07ebd29d71abe934dbb84c0443fba848c69ca258
amalfouda <109471354+amalfouda@users.noreply.github.com>
2022-10-23 17:17:35 +0200

    Add files via upload

---
+++ list main.cpp
@@ -0,0 +1,70 @@
+//
+//  main.cpp
+//  exam
+//
+//  Created by Amal Fouda on 22/10/2022.
+//
+
+#include <iostream>
+#include <vector>
+#include "list.h"
+using namespace std;
+
+void setvector(vector<int>&v1,int n)
+{
+    v1.push_back(n);
+}
+void insertAfter(vector<int>&v1,int first, int second)
+{
+    for (int i=0;i<v1.size();i++)
+    {
+        if(v1[i]==first)
+        {
+
+            v1.insert(begin(v1)+i+1, second);
+            
+        }
+    }
+   
+}
+void display(vector<int>v1)
+{
+    for(int i=0;i<v1.size();i++)
+        cout<<v1[i]<<endl;
+}
+
+    list creatlist(vector<int>v2)
+    {
+       
+        list l1;
+        l1.insertfirst(v2[0]);
+        for(int i=1;i<v2.size();i++)
+        {
+            if(l1.increaseocc(v2[i])==false)
+                l1.insertend(v2[i]);
+        }
+      
+        return l1;
+}
+
+int main() {
+    list l2;
+    vector<int>v;
+    setvector(v,1);
+    setvector(v,2);
+    setvector(v,3);
+    setvector(v,2);
+    setvector(v,4);
+    setvector(v,5);
+    setvector(v,2);
+    setvector(v,1);
+    setvector(v,2);
+    insertAfter(v,2, 9);
+    insertAfter(v,3, 70);
+    //display(v);
+    l2=creatlist(v);
+    l2.displaylist();
+    cout<<"----------------"<<endl;
+    l2.sum();
+    return 0;
+}
